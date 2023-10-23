a808e46a8a1d874d578f40367cc3a559388bf8c4
stdog210420 <lin23013@byui.edu>
2023-10-24 00:43:19 +0800

    added to my homepage

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 /* Declare and initialize global variables */
 
-    //Declare a const variable named templesElement that references the HTML div element that eventually will be populated with temple data.
+    //Declare a const variable named productElement that references the HTML div element that eventually will be populated with product data.
 const productElement = document.querySelector("#products");
-    //Declare a global empty array variable to store a list of temples named templeList.
+    //Declare a global empty array variable to store a list of products named templeList.
     // use let not const
 let productList= [];
 
-/* async displayTemples Function */
+/* async displayProducts Function */
 
-    //Declare a function expression using const named displayTemples that uses an arrow function
+    //Declare a function expression using const named displayproducts that uses an arrow function
     // to accept a list of temples as an array argument.
 const displayProducts = (products) =>{
-    //Process each temple in the temple array using a forEach method and do the following for each temple item:
+    //Process each product in the product array using a forEach method and do the following for each product item:
     products.forEach((product) => {
     //Create an HTML <article> element (createElement).
     const article= document.createElement("article");    
-    //Create an HTML <h3> element and add the temple's templeName property to this new element.
+    //Create an HTML <h3> element and add the product's productName property to this new element.
     const h3 = document.createElement("h3");   
     h3.textContent = product.productName;
     // create price element
@@ -24,27 +24,32 @@ const displayProducts = (products) =>{
     // create quantity element
     const quantityElement = document.createElement('p');
     quantityElement.textContent = `數量: ${product.quantity}`;
-    //Create an HTML <img> element and add the temple's imageUrl property to the src attribute and 
-    //the temple's location property to the alt attribute.
+    //Create an HTML <img> element and add the product's imageUrl property to the src attribute and 
+    //the product's location property to the alt attribute.
     const img = document.createElement("img"); 
     img.src = product.imageUrl;
     img.alt = product.class;
+    //Create button for each product
+    const button = document.createElement("button");
+    button.textContent = "加入購物車";
     //Append the <h3> element and the <img> element to the <article> element as children. (appendChild)
     article.appendChild(h3);
     article.appendChild(img);
     article.appendChild(priceElement);
     article.appendChild(quantityElement);
-    //Append the <article> element to the global templesElement variable declared in Step 2
-    productElement.appendChild(article);})
+    article.appendChild(button);
+    //Append the <article> element to the global productElement variable declared in Step 2
+    productElement.appendChild(article); })
+
 }
 
-/* async getTemples Function using fetch()*/
+/* async getProducts Function using fetch()*/
 
-    //Create another function expression called getTemples. Make it an async anonymous, arrow function.
+    //Create another function expression called getProducts. Make it an async anonymous, arrow function.
 const getProducts = async () => {
-    //In the function, declare a const variable named response that awaits the built-in fetch method calling the temple data, absolute URL given in Step 2 above.
+    //In the function, declare a const variable named response that awaits the built-in fetch method calling the product data, absolute URL given in Step 2 above.
     const response = await fetch("https://stdog210420.github.io/best_sale/products.json"); 
-    //Convert your fetch response into a JavaScript object (.json) and assign the result to the templeList global array variable you declared in Step 3 above. 
+    //Convert your fetch response into a JavaScript object (.json) and assign the result to the productList global array variable you declared in Step 3 above. 
     //Make sure the the execution of the code waits here as well until it finishes.
     if (response.ok) {
         // the API will send us JSON...but we have to convert the response before we can use it
@@ -53,7 +58,7 @@ const getProducts = async () => {
         //Make sure the the execution of the code waits here as well until it finishes
         //not use let, it's a local variable. Without the let or const prefix is assumed to be a global variable. 
         productList  = await response.json();
-        //The last statement in the getTemples function calls the displayTemples function and passes it the list of temples (templeList).
+        //The last statement in the getProducts function calls the displayProducts function and passes it the list of products (productList).
         displayProducts(productList);
         console.log(productList)
     }
@@ -61,44 +66,44 @@ const getProducts = async () => {
 
 /* reset Function */
 
-    //Clear the displayed list of temples.
+    //Clear the displayed list of products.
 const reset = () => {
-    //get templesElement 
+    //get productElement 
     const productElement = document.querySelector("#products");
     //clear all article elements
     productElement.innerHTML = "";
 }
 
-if (Array.isArray(productList)) {
+// if (Array.isArray(productList)) {
     // templeList 是一個陣列，可以使用 filter
-    console.error("productList 是一個陣列。");
-} else {
-    console.error("productList 不是一個陣列。");
-}
+//     console.error("productList 是一個陣列。");
+// } else {
+//     console.error("productList 不是一個陣列。");
+// }
 
 /* sortBy Function */
 
     //Declare a function expression named sortBy
-    //The function should accept a argument in a parameter named temples.
+    //The function should accept a argument in a parameter named products.
 const sortBy = (productList) => {
     //In this function, first call the reset function to clear the output.
     reset ();
     //Define a variable named filter that obtains the value of the HTML element with the ID of sortBy (The pull-down menu).
     const filter = document.querySelector("#sortBy").value;
     //Use a switch statement that uses the filter value as the selector responding to four (4) cases.
-    //For each case, call the displayTemples function using an filter statement that filters the temples parameter for the four options provided.
+    //For each case, call the displayProducts function using an filter statement that filters the products parameter for the four options provided.
     switch (filter) {
-        //"utah": filter for temples where the location contains "Utah" as a string.
-        case "utah":
-            const handbag =productList.filter((product) => product.class.includes("提袋"));
+        //"utah": filter for products where the location contains "束口提袋" as a string.
+        case "handbag":
+            const handbag =productList.filter((product) => product.class.includes("束口提袋"));
             displayProducts(handbag);
             break;
-        //"nonutah": filter for temples where the location does not contain "Utah" as a string.
-        case "notutah":
-            const notHandbag = productList.filter((product) => !product.class.includes("提袋"));
+        //"nonutah": filter for products where the location does not contain "束口提袋" as a string.
+        case "notHandbag":
+            const notHandbag = productList.filter((product) => !product.class.includes("束口提袋"));
             displayProducts(notHandbag);
             break;
-        //"all": no filter. Just use temples as the argument.
+        //"all": no filter. Just use products as the argument.
         case "all":
             displayProducts(productList);
             break;
@@ -107,7 +112,32 @@ const sortBy = (productList) => {
 /* Event Listener */
 
     //Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function 
-    //and sends a arrow function result with the templeList as the argument.
+    //and sends a arrow function result with the productList as the argument.
     document.querySelector("#sortBy").addEventListener("change", () => {sortBy(productList)});
 
-getProducts();
+    getProducts();
+
+// Add shopping cart button functionality
+const addToCartButton = document.getElementById("addToCart");
+const cartItems = document.getElementById("cart-items");
+const totalCost = document.getElementById("total");
+let cart = [];
+let totalPrice = 0;
+//click button function
+function addToCart(productId) {
+    const product = productList.find((item) => item.productId === productId);
+    // add product to shopping cart
+    cart.push(product);
+
+    // update quantity
+    product.quantity++;
+
+  // Create a new list element
+const listItem = document.createElement("li");
+listItem.textContent = `商品編號： ${product.productId}, 商品名稱: ${product.name}, 價格: $${product.price}, 數量: ${product.quantity}`;
+  // add listItem to shopping cart
+cartItems.appendChild(listItem);
+
+  // update total price
+totalPrice += product.price * product.quantity;
+totalCost.textContent = `總金額: $${totalPrice}`;}
